c5a48947c71f567da7150defdfcab732f205089b
MiddleSchoolStudent <middleschoolstudent00@gmail.com>
2024-12-15 11:55:55 -0500

    recaptcha video

--- tests/tests/antibots/recaptcha.spec.ts
+++ tests/tests/antibots/recaptcha.spec.ts
@@ -2,10 +2,14 @@ import { expect, test } from '../global-setup';
 import { sleep } from '../utils';
 
 test('test reCAPTCHA v3 on antcpt', async ({ page }) => {
-    await page.goto('https://antcpt.com/score_detector/');
+    test.setTimeout(120_000);
+
+    await page
+        .goto('https://antcpt.com/score_detector/', { timeout: 60_000 })
+        .catch(() => {});
 
     let score: number = 0;
-    for (let n = 0; n < 3; n++) {
+    for (let n = 0; n < 10; n++) {
         const yourScoreText = await page
             .locator('text=Your score is:')
             .innerText();
