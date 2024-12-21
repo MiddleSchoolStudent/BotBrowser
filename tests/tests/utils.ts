d972592043201240257074c1397f6c6608a3df2c
MiddleSchoolStudent <middleschoolstudent00@gmail.com>
2024-12-21 14:48:10 -0500

    px test result

--- tests/tests/utils.ts
+++ tests/tests/utils.ts
@@ -4,11 +4,27 @@ export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export async function waitForFrame({ page, url }: { page: Page; url: string }) {
+export async function waitForFrame(
+    options: { page: Page; url: string } | { page: Page; title: string },
+) {
     let frame: Frame | undefined;
     while (!frame) {
         await sleep(1000);
-        frame = page.frames().find((frame) => frame.url().startsWith(url));
+
+        const frames = options.page.frames();
+        for (const f of frames) {
+            if ('url' in options) {
+                if (f.url() === options.url) {
+                    frame = f;
+                    break;
+                }
+            } else {
+                if ((await f.title()) === options.title) {
+                    frame = f;
+                    break;
+                }
+            }
+        }
     }
 
     return frame;
