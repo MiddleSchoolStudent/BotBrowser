100f0c17794fcf6aa848dc562dce194221c8fad3
MiddleSchoolStudent <106871516+MiddleSchoolStudent@users.noreply.github.com>
2025-04-26 15:06:18 -0400

    Додати демонстрацію Hotmail (funcaptcha)

--- tests/tests/antibots/funcaptcha.spec.ts
+++ tests/tests/antibots/funcaptcha.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '../global-setup';
-import { waitForFrame } from '../utils';
+import { sleep, waitForFrame } from '../utils';
 
 test('blizzard', async ({ page }) => {
     await page.goto(`https://account.battle.net/creation/flow/creation-full`);
@@ -52,4 +52,75 @@ test('roblox', async ({ page }) => {
 
     await page.locator('button#signup-button').click();
     await page.waitForNavigation({ url: 'https://www.roblox.com/home' });
+});
+
+test('hotmail', async ({ page }) => {
+    await page.goto(`https://signup.live.com/signup`);
+    await page.locator('span#liveSwitch').click();
+    await page.locator('select#domainSelect').selectOption({
+        value: 'hotmail.com',
+    });
+
+    const username =
+        Math.random().toString(36).substring(2).replace(/^\d+/g, '') + Math.random().toString(36).substring(2);
+    const password = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(5) + 'AC?_';
+
+    await page.locator('input#usernameInput').pressSequentially(username, {
+        delay: 20,
+    });
+    await page.keyboard.press('Enter');
+    await page.locator('input#Password').pressSequentially(password, {
+        delay: 20,
+    });
+    await page.keyboard.press('Enter');
+
+    await page.locator('input#firstNameInput').pressSequentially(
+        Math.random()
+            .toString(36)
+            .substring(2)
+            .replace(/[^a-zA-Z]/g, ''),
+        { delay: 20 }
+    );
+    await page.locator('input#lastNameInput').pressSequentially(
+        Math.random()
+            .toString(36)
+            .substring(2)
+            .replace(/[^a-zA-Z]/g, ''),
+        { delay: 20 }
+    );
+    await page.keyboard.press('Enter');
+
+    await page.locator('select#BirthMonth').selectOption({
+        value: '1',
+    });
+    await page.locator('select#BirthDay').selectOption({
+        value: '1',
+    });
+    await page.locator('input#BirthYear').pressSequentially('1998', {
+        delay: 20,
+    });
+    await sleep(2000);
+    await page.locator('button#nextButton').click();
+
+    await page
+        .locator('[data-testid="enforcementFrame"]')
+        .contentFrame()
+        .locator('iframe[title="Verification challenge"]')
+        .contentFrame()
+        .locator('iframe[title="Visual challenge"]')
+        .contentFrame()
+        .getByRole('button', { name: 'Solve puzzle' })
+        .click();
+
+    expect(
+        await page
+            .locator('[data-testid="enforcementFrame"]')
+            .contentFrame()
+            .locator('iframe[title="Verification challenge"]')
+            .contentFrame()
+            .locator('iframe[title="Visual challenge"]')
+            .contentFrame()
+            .getByText('(1 of 1)') // It means we have a great environment with only one wave.
+            .elementHandle()
+    ).toBeTruthy();
 });
