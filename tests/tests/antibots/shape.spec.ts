a62e43e844130d913773fed528e139fda4331f61
MiddleSchoolStudent <106871516+MiddleSchoolStudent@users.noreply.github.com>
2025-05-15 21:56:06 -0400

    Target test

--- tests/tests/antibots/shape.spec.ts
+++ tests/tests/antibots/shape.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '../global-setup';
-import { getDateFormatted } from '../utils';
+import { getDateFormatted, sleep } from '../utils';
 
 test('southwest', async ({ page }) => {
     const tomorrowDate = getDateFormatted(1);
@@ -15,4 +15,35 @@ test('southwest', async ({ page }) => {
     const apiResponseJson = await apiResponse.json();
 
     expect(apiResponseJson).toHaveProperty('data');
+});
+
+test('target', async ({ page }) => {
+    await page.goto('https://www.target.com/');
+    await page.locator('a#account-sign-in').click();
+    await page.locator('button >> text=Sign in or create account').click();
+    await page.locator('input#username').pressSequentially(Math.random().toString(36).substring(2) + '@gmail.com', {
+        delay: 20,
+    });
+    await page.keyboard.press('Enter');
+    await page.locator('input#firstname').pressSequentially(
+        Math.random()
+            .toString(36)
+            .substring(2)
+            .replace(/[^a-zA-Z]/g, ''),
+        { delay: 20 }
+    );
+    await page.locator('input#lastname').pressSequentially(
+        Math.random()
+            .toString(36)
+            .substring(2)
+            .replace(/[^a-zA-Z]/g, ''),
+        { delay: 20 }
+    );
+    await page.locator('input#password-checkbox').click();
+    await page.locator('input#password').pressSequentially(Math.random().toString(36).substring(2) + 'AC?_', {
+        delay: 20,
+    });
+    await sleep(2000);
+    await page.locator('button#createAccount').click();
+    await page.locator('h1 >> text=Verification code sent').waitFor();
 });
