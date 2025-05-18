1088230a53073e58d6c226b1ebe20b1cd30e1af5
MiddleSchoolStudent <106871516+MiddleSchoolStudent@users.noreply.github.com>
2025-05-17 21:01:06 -0400

    Fingerprint Pro demo

--- tests/tests/antibots/fingerprintjs.spec.ts
+++ tests/tests/antibots/fingerprintjs.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '../global-setup';
+import { sleep } from '../utils';
 
 test('botdetection', async ({ page }) => {
     await page.goto('https://fingerprint.com/products/bot-detection/');
@@ -8,4 +9,14 @@ test('botdetection', async ({ page }) => {
             timeout: 20_000,
         })
     ).toBeTruthy();
+});
+
+test('playground', async ({ page }) => {
+    await page.goto('https://demo.fingerprint.com/playground');
+
+    await sleep(5_000);
+    for (let i = 0; i < 20; i++) {
+        await page.mouse.wheel(0, 50);
+        await sleep(300);
+    }
 });
