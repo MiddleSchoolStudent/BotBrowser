99d62a9913a518f6cc85e0725b400406dec672ad
MiddleSchoolStudent <106871516+MiddleSchoolStudent@users.noreply.github.com>
2025-07-10 01:36:41 +0800

    TAROM demo

--- tests/tests/antibots/incapsula.spec.ts
+++ tests/tests/antibots/incapsula.spec.ts
@@ -14,3 +14,17 @@ test('copaair', async ({ page }) => {
     const apiResponse = await apiResponsePromise;
     expect(apiResponse.status()).toBe(200);
 });
+
+test('tarom', async ({ page }) => {
+    const futureDate = getDateFormatted(1);
+    await page.goto(
+        `https://digital.tarom.ro/booking?lang=en-GB&search=%7B%22travelers%22%3A%5B%7B%22passengerTypeCode%22%3A%22ADT%22%7D%5D%2C%22commercialFareFamilies%22%3A%5B%22EUROPE%22%5D%2C%22itineraries%22%3A%5B%7B%22departureDateTime%22%3A%22${futureDate}%22%2C%22originLocationCode%22%3A%22LCG%22%2C%22destinationLocationCode%22%3A%22OTP%22%7D%5D%7D&portalFacts=%5B%7B%22key%22%3A%22currency%22%2C%22value%22%3A%22EUR%22%7D%2C%7B%22key%22%3A%22channel%22%2C%22value%22%3A%22desktop%22%7D%5D`
+    );
+    const apiResponse = await page.waitForResponse(
+        (response) =>
+            response.request().method() === 'POST' &&
+            response.url().includes('https://api-des.tarom.ro/v2/search/air-bounds') &&
+            response.status() === 200
+    );
+    expect(apiResponse.status()).toBe(200);
+});
